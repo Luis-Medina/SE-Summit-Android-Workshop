dd5b28b88ac483366eb0178353ec8c575a21cc5c
Nick Capurso <capurso.nick@gmail.com>
2019-07-21 13:05:03 -0400

    Bump to AndroidX

--- app/src/main/java/com/summit/summitproject/LoginActivity.java
+++ app/src/main/java/com/summit/summitproject/LoginActivity.java
@@ -3,7 +3,7 @@ package com.summit.summitproject;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
