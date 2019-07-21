83aeffc56609e66f0a65b18962cb780c79b50f28
Nick Capurso <capurso.nick@gmail.com>
2019-07-21 13:01:45 -0400

    Bump to AndroidX

--- app/src/main/java/com/summit/summitproject/LoginActivity.java
+++ app/src/main/java/com/summit/summitproject/LoginActivity.java
@@ -1,16 +1,13 @@
 package com.summit.summitproject;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
